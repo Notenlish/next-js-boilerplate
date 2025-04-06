0c1300dec429f27bd3eb8bac4d85dacecce29d21
Notenlish <71970100+Notenlish@users.noreply.github.com>
2025-04-06 14:26:25 +0300

    Update page.tsx

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -2,7 +2,8 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        Hello world! You can edit this page on my-app/src/app/page.tsx
+        Hello world! You can edit this page on{" "}
+        <span className=" text-accent">my-app/src/app/page.tsx</span>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
     </div>
